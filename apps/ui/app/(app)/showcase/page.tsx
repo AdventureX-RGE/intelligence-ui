b7df953ef9da47030ca1b0f1196db19b6ab74b78
Jayden <jayden@JaydendeMacBook-Air.local>
2025-03-17 00:12:48 +0800

    feat: add some style

--- apps/ui/app/(app)/showcase/page.tsx
+++ apps/ui/app/(app)/showcase/page.tsx
@@ -12,13 +12,13 @@ export const metadata: Metadata = {
 }
 
 export default async function Page() {
-  const res = await fetch(
-    "https://raw.githubusercontent.com/justdlabs/showcase/refs/heads/main/sites.json",
-    {
-      next: { revalidate: 3600 },
-    },
-  )
-  const sites = await res.json()
+  // const res = await fetch(
+  //   "https://raw.githubusercontent.com/justdlabs/showcase/refs/heads/main/sites.json",
+  //   {
+  //     next: { revalidate: 3600 },
+  //   },
+  // )
+  // const sites = await res.json()
   return (
     <>
       <Header>
@@ -26,7 +26,7 @@ export default async function Page() {
         <span className="text-muted-fg tracking-tight">case</span>
       </Header>
       <Container className="py-4 sm:py-16">
-        <ListSites sites={sites} />
+        <ListSites sites={[]} />
       </Container>
     </>
   )
