675ced78f738ef3e26a0821ce5d5f771735cc94f
Macw07 <87552695+Macw07@users.noreply.github.com>
2025-03-24 15:24:34 -0400

    perf(typography): convert text size from em to rem

--- apps/ui/components/ui/typography.tsx
+++ apps/ui/components/ui/typography.tsx
@@ -1,19 +1,18 @@
-import { forwardRef } from "react"
 import { type VariantProps, tv } from "tailwind-variants"
 
 const typographyStyles = tv({
   base: "",
   variants: {
     variant: {
-      h1: "font-patika-medium text-[2em] leading-[2.25em]",
-      h2: "font-patika-bold text-[1.5em] leading-[2em]",
-      h3: "font-patika-medium text-[1.25em] leading-[1.5em]",
-      paragraph: "font-patika-regular text-[1.25em] leading-[1.75em]",
-      body: "font-patika-regular text-[1em] leading-[1.5em]",
-      bodyEmphasized: "font-patika-medium text-[1em] leading-[1.5em]",
-      button: "font-patika-medium text-[1em] leading-[1.5em]",
-      note: "font-patika-regular text-[0.875em] leading-[1.25em]",
-      caption: "font-orbix-regular text-[1em] leading-[1.25em]",
+      h1: "font-patika-medium text-[2rem] leading-[2.25rem]",
+      h2: "font-patika-bold text-[1.5rem] leading-[2rem]",
+      h3: "font-patika-medium text-[1.25rem] leading-[1.5rem]",
+      paragraph: "font-patika-regular text-[1.25rem] leading-[1.75rem]",
+      body: "font-patika-regular text-[1rem] leading-[1.5rem]",
+      bodyEmphasized: "font-patika-medium text-[1rem] leading-[1.5rem]",
+      button: "font-patika-medium text-[1rem] leading-[1.5rem]",
+      note: "font-patika-regular text-[0.75rem] leading-[1.25rem]",
+      caption: "font-orbix-regular text-[1rem] leading-[1.25rem]",
     },
   },
   defaultVariants: {
@@ -28,15 +27,13 @@ type TypographyProps = {
   as?: "p" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "span" | "div"
 }
 
-const Typography = forwardRef<HTMLElement, TypographyProps>(
-  ({ className, variant, as: Tag = "p", children, ...props }, ref) => {
-    return (
-      <Tag className={typographyStyles({ variant, className })} {...props}>
-        {children}
-      </Tag>
-    )
-  },
-)
+const Typography = ({ className, variant, as: Tag = "p", children, ...props }: TypographyProps) => {
+  return (
+    <Tag className={typographyStyles({ variant, className })} {...props}>
+      {children}
+    </Tag>
+  )
+}
 
 Typography.displayName = "Typography"
 
